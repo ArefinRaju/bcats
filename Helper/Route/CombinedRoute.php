11c0f1af21a9c60aac4f0cc7e9274d58ec464eaf
Md. Arefin Ullah <arefin.ullah@xen.works>
2021-02-07 03:38:14 +0600

    Saving purpose

---
+++ Helper/Route/CombinedRoute.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Helper\Route;
+
+
+use Helper\Constants\CRUD;
+use Illuminate\Support\Facades\Route;
+
+class CombinedRoute
+{
+    public static function resourceRoute(
+        string $slug,
+        string $controller,
+        array $middlewares,
+        array $rules = []
+    ): void {
+        $actionTaggedSlug = $slug.'/{id}[/{action}]';
+
+        if (!self::ActionAllowed($rules, CRUD::RETRIEVE)) {
+            Route::get($actionTaggedSlug,
+                self::generateRouteOptions($slug, $controller, CRUD::RETRIEVE, $middlewares));
+        }
+    }
+
+    private static function ActionAllowed(array $rules, string $action): bool
+    {
+        if (isset($rules['excluded'])) {
+            return is_array($rules['excluded']) ? in_array($action, $rules['excluded']) : $action == $rules['excluded'];
+        }
+
+        return false;
+    }
+
+    private static function generateRouteOptions(
+        string $slug,
+        string $controller,
+        string $action,
+        array $middlewares
+    ): array {
+        $options = [
+            'uses' => $controller.'@'.$action,
+            'as' => $slug.'_'.$action
+        ];
+
+        if (count($middlewares) != 0) {
+            $options['middleware'] = $middlewares;
+        }
+
+        return $options;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Helper\Route\CombinedRoute;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/',	'ProductController@retrieve');
