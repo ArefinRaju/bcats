3e5d8097cd527ae2b97a9e8745137996dadc8353
Mohammad Ali <mdali89a@gmail.com>
2021-02-25 22:39:01 +0600

    login and master page templeting

---
+++ resources/views/admin/partials/_style.blade.php
@@ -0,0 +1,11 @@
+	<!--begin::Fonts-->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+	<!--end::Fonts-->
+	<!--begin::Page Vendors Styles(used by this page)-->
+	<link href="{{asset('admin')}}/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css?v=7.0.4" rel="stylesheet" type="text/css" />
+	<!--end::Page Vendors Styles-->
+	<!--begin::Global Theme Styles(used by all pages)-->
+	<link href="{{asset('admin')}}/assets/plugins/global/plugins.bundle.css?v=7.0.4" rel="stylesheet" type="text/css" />
+	<link href="{{asset('admin')}}/assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.4" rel="stylesheet" type="text/css" />
+	<link href="{{asset('admin')}}/assets/css/style.bundle.css?v=7.0.4" rel="stylesheet" type="text/css" />
+	<!--end::Global Theme Styles-->
