a53c768bbe2a6559846d58cc1a7bf94fa387ba25
Mohammad Ali <mdali89a@gmail.com>
2021-02-25 21:55:48 +0600

    login page added

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login',
+    function () {
+        return view('admin.layouts.login');
+    }
+);
 Route::get('/',
     function () {
         return view('admin.dashboard');
