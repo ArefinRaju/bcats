460e6e6438761aaa4c13d56c756c7022e4375fb7
Md. Arefin Ullah <arefin.ullah@xen.works>
2021-02-26 21:08:10 +0600

    - Enum const added

---
+++ Helper/Constants/Enum.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Helper\Constants;
+
+
+class Enum extends Holder
+{
+    public const KG    = 'kg';
+    public const TON   = 'ton';
+    public const FEET  = 'ft';
+    public const METER = 'meter';
+    public const PIECE = 'pc';
+}
--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Material;
 use Helper\Constants\CommonValidations as V;
+use Helper\Constants\Enum;
 use Helper\Core\HelperController;
 use Helper\Repo\MaterialRepository;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class MaterialController extends HelperController
 {
@@ -19,7 +22,14 @@ class MaterialController extends HelperController
         $this->setResource(Material::class);
         $this->commonValidationRules = [
             'name' => [V::REQUIRED, V::TEXT],
-            'enum' => [V::REQUIRED, V::TEXT],
+            'enum' => [V::REQUIRED, V::TEXT, Rule::in(Enum::values())],
         ];
+    }
+
+    public function create(Request $request, string $action = null)
+    {
+        $material = $this->validateCherryPickAndAssign($request, $this->commonValidationRules, new Material());
+        $this->repo->save($material);
+        dd($material->toArray());
     }
 }
