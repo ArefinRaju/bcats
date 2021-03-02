489972b0476918705c9096e97e32b89f97717267
Mohammad Ali <mdali89a@gmail.com>
2021-03-02 17:52:42 +0600

    header and dashboard modify

--- resources/views/admin/partials/_header.blade.php
+++ resources/views/admin/partials/_header.blade.php
@@ -4,8 +4,8 @@
         <!--begin::Header Logo-->
         <div class="header-logo">
             <a href="index.html">
-                <img alt="Logo" src="{{asset('admin')}}/assets/media/logos/logo-letter-9.png" class="logo-default max-h-40px" />
-                <img alt="Logo" src="{{asset('admin')}}/assets/media/logos/logo-letter-1.png" class="logo-sticky max-h-40px" />
+                <img alt="Logo" src="{{asset('admin')}}/assets/media/logos/bcats-big.png" class="logo-default max-h-40px" />
+                <img alt="Logo" src="{{asset('admin')}}/assets/media/logos/bcats-big.png" class="logo-sticky max-h-40px" />
             </a>
         </div>
         <!--end::Header Logo-->
