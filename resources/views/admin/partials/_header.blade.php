88a236420d4468b2d3f16f290faaa436bed78425
Mohammad Ali <mdali89a@gmail.com>
2021-03-17 20:59:24 +0600

    header modify

--- resources/views/admin/partials/_header.blade.php
+++ resources/views/admin/partials/_header.blade.php
@@ -724,7 +724,7 @@
             <div class="topbar-item" data-toggle="dropdown" data-offset="0px,0px">
                 <div class="btn btn-icon btn-hover-transparent-white d-flex align-items-center btn-lg px-md-2 w-md-auto">
                     <span class="text-white opacity-70 font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-                    <span class="text-white opacity-90 font-weight-bolder font-size-base d-none d-md-inline mr-4">{{Auth::user()->name}}</span>
+                    <span class="text-white opacity-90 font-weight-bolder font-size-base d-none d-md-inline mr-4">{{Auth::user()->name??''}}</span>
                     <!-- <span class="symbol symbol-35">
                         <span class="symbol-label text-white font-size-h5 font-weight-bold bg-white-o-30">S</span>
                     </span> -->
@@ -741,7 +741,7 @@
                     </div>
                     <!--end::Symbol-->
                     <!--begin::Text-->
-                    <div class="text-dark m-0 flex-grow-1 mr-3 font-size-h5">{{Auth::user()->name}}</div>
+                    <div class="text-dark m-0 flex-grow-1 mr-3 font-size-h5">{{Auth::user()->name ??''}}</div>
                     <!-- <span class="label label-light-success label-lg font-weight-bold label-inline">3 messages</span> -->
                     <!--end::Text-->
                 </div>
