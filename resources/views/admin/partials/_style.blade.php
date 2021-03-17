cf3a30ae1df494a22fa28ca648b01213060a1f41
Mohammad Ali <mdali89a@gmail.com>
2021-03-17 15:56:31 +0600

    error msg and toastr added

--- resources/views/admin/partials/_style.blade.php
+++ resources/views/admin/partials/_style.blade.php
@@ -9,3 +9,4 @@
 	<link href="{{asset('admin')}}/assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.4" rel="stylesheet" type="text/css" />
 	<link href="{{asset('admin')}}/assets/css/style.bundle.css?v=7.0.4" rel="stylesheet" type="text/css" />
 	<!--end::Global Theme Styles-->
+	<link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
