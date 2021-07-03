158a59c60b580ec0d017a81f476877ad4eed9614
Md Mridul <70252226+mdmridul6@users.noreply.github.com>
2021-07-03 14:32:09 +0600

    Revert "unknown Change"
    This reverts commit f314cbef41bdf886a7b37433ae0ac21d056cf355.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Bcats'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', null),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC+6',
+    'timezone' => 'UTC',
 
     /*
     |--------------------------------------------------------------------------
