7996fba9506a4c36b6d795a73dbadfecc338a5bc
Arefin <elitearefin@hotmail.com>
2021-10-04 15:05:53 +0600

    Debit list reworked

--- Helper/Repo/MaterialHistoryRepository.php
+++ Helper/Repo/MaterialHistoryRepository.php
@@ -53,6 +53,17 @@ class MaterialHistoryRepository extends EntityRepository
     public function debitList(int $perPage = null, int $page = null)
     {
         return MaterialHistory::where('project_id', Request()->user()->project_id)
+                              ->select(
+                                  'material_histories.id as id',
+                                  'material_histories.user_name',
+                                  'material_histories.material_name',
+                                  'material_histories.total',
+                                  'material_histories.debit',
+                                  'material_histories.used',
+                                  'material_histories.comment',
+                                  'materials.enum',
+                                  'material_histories.updated_at'
+                              )
                               ->leftJoin('materials', 'material_histories.material_id', 'materials.id')
                               ->whereNotIn('debit', [0.00])
                               ->orderBy('material_histories.id', 'desc')
